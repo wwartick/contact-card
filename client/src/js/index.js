feaaa1f9ee51fc08d754bcbb9a01d29ac8ce852d
wwartick <104100597+wwartick@users.noreply.github.com>
2022-11-22 15:21:36 -0800

    Add files via upload

---
+++ client/src/js/index.js
@@ -0,0 +1,17 @@
+import "./form";
+import "./submit";
+import { Tooltip, Toast, Popover } from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Logo from '../images/logo.png';
+import Bear from '../images/bear.png';
+import Dog from '../images/dog.png';
+
+window.addEventListener('load', function () {
+    document.getElementById('logo').src = Logo;
+    document.getElementById('bearThumbnail').src = Bear;
+    document.getElementById('dogThumbnail').src = Dog;
+  });
+
+    // Import CSS files
+    import "../css/index.css";
