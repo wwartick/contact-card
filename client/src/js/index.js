e0feb085abe00436f7b67f73112356071059bcde
wwartick <willwartick@gmail.com>
2022-11-22 16:16:28 -0800

    integrated indexeddb

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -6,8 +6,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
+import {initdb} from './database';
 
-window.addEventListener('load', function () {
+ window.addEventListener('load', function () {
+    initdb();
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
